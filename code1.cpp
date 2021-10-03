2909c9db47ad168ac37d9c42648dbff24adb2936
Kumar Shubham <kumar.shubham@appdynamics.com>
2021-10-03 10:51:11 +0530

    Adding cp template

---
+++ code1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout<<"Hello world"<<endl;
+
+    return 0;
+}
